94339c53aba5e0d5cc1dcc2a8e506573649df738
Jonathan <jonacom@lissismore.com>
2016-09-08 11:20:46 -0400

    template function max

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+inline T const& Max(T const& a, T const& b) {
+  return a < b ? b : a;
+}
+
+
+int main() {
+
+  int i = 10;
+  int j = 20;
+  cout << "Max(10, 20): " << Max(i, j) << endl;
+
+  float a = 10.5;
+  float b = 20.5;
+  cout << "Max(10.5, 20.5): " << Max(a, b) << endl;
+  
+  return 0;
+}
