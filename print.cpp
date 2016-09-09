2ad7815abad964736ee8cb823e52e09686bbc7d3
Jonathan <jonacom@lissismore.com>
2016-09-09 13:10:01 -0400

    add comments and more examples

--- print.cpp
+++ print.cpp
@@ -3,10 +3,25 @@
 
 using namespace std;
 
-template<typename Input>
-void print(Input in) { cout << in << endl; };
+/* template print function which can take any type input */
+template<typename Type>
+void print(Type in) { cout << in << endl; };
 
 int main() {
+
+  /* strings */
   print("Hello, World!");
+  string hello = "Hello, World!";
+  print(hello);
+
+  /* integers */
+  print(5);
+
+  /* floats */
+  print(1.111234);
+
+  /* chars */
+  print('a');
+
   return 0;
 }
